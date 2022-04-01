efaba3c953466281a978d725f28e3a386907d894
Prabhapalraj <prabhapalraj92@gmail.com>
2022-04-01 21:10:56 +0530

    conflict

--- src/javprabha/Abst.java
+++ src/javprabha/Abst.java
@@ -4,6 +4,7 @@ public abstract class Abst {
 	public void empid() {
 		// TODO Auto-generated method stub
 System.out.println("empid");
+System.out.println("haii");
 	}
 	public abstract void empname();
 		// TODO Auto-generated method stub
